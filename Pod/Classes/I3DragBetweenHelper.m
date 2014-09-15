42ff578ca3691404c1bbf8cfed28919ce41aca0e
Ryan Dignard <ryan@shoptap.it>
2014-09-15 16:20:46 -0700

    fix greedy scrollview stealing my touches

--- Pod/Classes/I3DragBetweenHelper.m
+++ Pod/Classes/I3DragBetweenHelper.m
@@ -8,7 +8,7 @@
 
 #import "I3DragBetweenHelper.h"
 
-@interface I3DragBetweenHelper()
+@interface I3DragBetweenHelper() <UIGestureRecognizerDelegate>
 
 /* Redeclaration of 'private' properties */
 
@@ -96,6 +96,26 @@
 
 
 @implementation I3DragBetweenHelper
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    return YES;
+}
+
+- (BOOL) gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return NO;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldBeRequiredToFailByGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return NO;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    return YES;
+}
 
 
 -(void) reloadCellInContainer:(UIView*) view atIndexPaths:(NSArray*) paths{
@@ -1012,6 +1032,7 @@
     SEL dragSel = @selector(handlePan:);
     
     self.currentGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:dragSel];
+    self.currentGestureRecognizer.delegate = self;
     [_superview addGestureRecognizer:self.currentGestureRecognizer];
 
 }
